5e9ac674b12e861d1ffd457178fea0e8debb8261
Mary Saray (Sah-Rye) <82470838+marysaray@users.noreply.github.com>
2021-11-07 16:39:37 -0800

    Scaffolded ASP.NET Identity into project (#17)

---
+++ InteractiveCultureAndLanguages/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using InteractiveCultureAndLanguages.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(InteractiveCultureAndLanguages.Areas.Identity.IdentityHostingStartup))]
+namespace InteractiveCultureAndLanguages.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
